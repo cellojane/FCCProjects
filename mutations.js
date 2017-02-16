167862c8ce62b216a296195aedd5cb434eaba35c
Jane <jane.em.carter@gmail.com>
2017-02-16 13:35:36 -0500

    Update mutations.js

--- mutations.js
+++ mutations.js
@@ -1,18 +1,30 @@
 
 function mutation(arr) {
-  arr[0] = arr[0].toLowerCase().split("").sort().join("");
-  arr[1] = arr[1].toLowerCase().split("").sort();
+  arr[0] = arr[0].toLowerCase();
+  arr[1] = arr[1].toLowerCase().split("");
     /*if (0 <= arr[0].indexOf(arr[1].join(""))){
       return true;
     }*/
-    for (var i = 0; i < arr[1].length; i++) {
-      return 0 <= arr[0].indexOf(arr[1][i]); 
-    }
+    var index = [];
+    var reject = []; 
+  for (var i = 0; i < arr[1].length; i++) {
 
+      index.push(arr[0].indexOf(arr[1][i])); 
+
+    }
+    for (var j = 0; j < index.length; j++) {
+      if (index[j] === 0) {
+        reject = index.shift();
+      }
+    }
+    var singleIndex = index.reduce(function(previousVal, currentVal) {
+  return previousVal * currentVal;
+});
+    return singleIndex >= 0;
   }
 
   /*return 0 <= arr[0].indexOf(arr[1]);*/
 
-  
 
 mutation(["hello", "hey"]);
+
